3e68838cb7acd6cd8439f1ceced0fea1ac1d42cb
Villeneve Oliveira <villeneveoliveira@gmail.com>
2026-08-01 19:02:09 +0000

    change number os layers

--- src/models.py
+++ src/models.py
@@ -21,41 +21,36 @@ class Critic(nn.Module):
         return self.toMap(x)
 
 class Generator(nn.Module):
-    def __init__(self, lenght=5):
+    def __init__(self, lenght=5, init_ch=16):
         super().__init__()
         self.encoder = nn.Sequential(
-            # 256 -> 128 RF=4 jmp=2
-            setKaiming_(nn.Conv2d(3,32,4,2,1,padding_mode='reflect')),
-            nn.InstanceNorm2d(32),
+            setKaiming_(nn.Conv2d(3,init_ch,4,2,1,padding_mode='reflect')),
+            nn.InstanceNorm2d(init_ch),
             nn.ReLU(),
-            # 128 -> 64 RF=4+(4-1)*2=10 jmp=4
-            setKaiming_(nn.Conv2d(32,64,4,2,1,padding_mode='reflect')),
-            nn.InstanceNorm2d(64),
+            setKaiming_(nn.Conv2d(init_ch,init_ch*2,4,2,1,padding_mode='reflect')),
+            nn.InstanceNorm2d(init_ch*2),
             nn.ReLU(),
-            # 64 -> 32 RF=10+(4-1)*4=22 jmp=8
-            setKaiming_(nn.Conv2d(64,128,4,2,1,padding_mode='reflect')),
-            nn.InstanceNorm2d(128),
+            setKaiming_(nn.Conv2d(init_ch*2,init_ch*4,4,2,1,padding_mode='reflect')),
+            nn.InstanceNorm2d(init_ch*4),
             nn.ReLU(),
-            # 32 -> 16 RF=22+(4-1)*8=46 jmp=16
-            setKaiming_(nn.Conv2d(128,256,4,2,1,padding_mode='reflect')),
-            nn.InstanceNorm2d(256),
+            setKaiming_(nn.Conv2d(init_ch*4,init_ch*8,4,2,1,padding_mode='reflect')),
+            nn.InstanceNorm2d(init_ch*8),
             nn.ReLU(),
         )
         self.bottleneck = nn.Sequential(
-            # 32 -> 32 RF=22+(3-1)*8=38 jmp=8
-            *[RConv2d(256) for _ in range(lenght)]
+            *[RConv2d(init_ch*8) for _ in range(lenght)]
         )
         self.decoder = nn.Sequential(
             nn.UpsamplingNearest2d(scale_factor=2),
-            ConvNormBlock(256,128,3,1,1),
+            ConvNormBlock(8*init_ch,4*init_ch,3,1,1),
             nn.UpsamplingNearest2d(scale_factor=2),
-            ConvNormBlock(128,64,3,1,1),
+            ConvNormBlock(4*init_ch,2*init_ch,3,1,1),
             nn.UpsamplingNearest2d(scale_factor=2),
-            ConvNormBlock(64,32,3,1,1),
+            ConvNormBlock(2*init_ch,init_ch,3,1,1),
             nn.UpsamplingNearest2d(scale_factor=2),
-            ConvNormBlock(32,16,3,1,1),
+            ConvNormBlock(init_ch,init_ch//2,3,1,1),
         )
-        self.toRGB = setXavier_(nn.Conv2d(16,3,1))
+        self.toRGB = setXavier_(nn.Conv2d(init_ch//2,3,1))
     def forward(self, x:torch.Tensor):
         x = self.encoder(x)
         x = self.bottleneck(x)
