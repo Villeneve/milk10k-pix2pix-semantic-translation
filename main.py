b68ea16fa93541cc1b7a6af3597067d827345b5d
Villeneve Oliveira <villeneveoliveira@gmail.com>
2026-08-04 14:11:25 +0000

    add complete patchNCE

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ tr = tt.Compose([
     tt.ToDtype(torch.float32,scale=True),
     tt.Normalize([.5]*3,[.5]*3)
 ])
-dataset = load_dataset(64,True,4)
+dataset = load_dataset(32,True,4)
 
 #%%
 gpu = torch.device('cuda:1')
@@ -29,12 +29,12 @@ opt = [
     torch.optim.Adam(
         gen.parameters(),
         2e-4,
-        (0.,.9),
+        (.5,.99),
     ),
     torch.optim.Adam(
         crit.parameters(),
         2e-4,
-        (0.,.9)
+        (.5,.99)
     )
 ]
 rec_loss_g = None
@@ -72,15 +72,28 @@ for epoch in range(5000):
         adv_loss_c_.backward()
         opt[1].step()
 
+
         setGrad_(crit,False)
         fake_clin = gen(derm)
         cut_fake = gen.encoder(fake_clin).flatten(2).permute(0,2,1)
         cut_fake = F.normalize(cut_fake,2,-1)
-        
-        matrix = (cut_fake@cut_fake.transpose(-1,-2))/0.1
-        cut_loss_ = 0
-        for i in range(fake_clin.size(0)):
-            cut_loss_ += F.cross_entropy(matrix[i],torch.arange(256,device=gpu))/fake_clin.size(0)
+        with torch.no_grad():
+            cut_true = F.normalize(gen.encoder(derm).flatten(2),2,1)
+        matrix = (cut_fake@cut_true)/0.1
+        N, HW, _ = matrix.size()
+        matrix = matrix.view(N*HW,HW)
+        cut_loss_ = F.cross_entropy(matrix,torch.arange(256,device=gpu).repeat(N))
+
+        idt_clin = gen(clin)
+        cut_fake_idt = F.normalize(gen.encoder(idt_clin).flatten(2).permute(0,2,1), 2, -1)
+        with torch.no_grad():
+            cut_true_idt = F.normalize(gen.encoder(clin).flatten(2), 2, 1)
+        matrix_idt = (cut_fake_idt @ cut_true_idt) / 0.1
+        N2, HW2, _ = matrix_idt.shape
+        target_idt = torch.arange(HW2, device=gpu).repeat(N2)
+        cut_loss_Y = F.cross_entropy(matrix_idt.reshape(N2*HW2, HW2), target_idt)
+        cut_loss_ += cut_loss_Y
+
         fake_logits = crit(torch.cat([derm,fake_clin],1))
         # rec_loss_g_ = 100*(clin-fake_clin).abs().mean()
         adv_loss_g_ = -fake_logits.mean()
